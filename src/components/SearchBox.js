4324317cfeadd6a69a66348fdf2dc577299ec0b1
ammar <ammar.botonjic@convergent-usa.com>
2020-02-08 14:39:01 +0100

    init

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,8 +3,10 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+      <label for="s">Search</label>
       <input
         className='pa3 ba b--green bg-lightest-blue'
+        id="s"
         type='search'
         placeholder='search robots'
         onChange={searchChange}
